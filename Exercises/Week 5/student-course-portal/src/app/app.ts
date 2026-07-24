8c60ff9d93ddd4127a10e668aa6b4050b66e09fc
Akhilsucaj <akhiljoshisucaj@gmail.com>
2026-07-24 12:56:34 +0530

    Angular Student Course Portal Hands On 4

--- Exercises/Week 5/student-course-portal/src/app/app.ts
+++ Exercises/Week 5/student-course-portal/src/app/app.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Header } from './components/header/header';
-import { CourseList } from './pages/course-list/course-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, CourseList, RouterOutlet],
+  imports: [
+    Header,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
